01a2d86083b1388e34a7bfba6e041c6c0704204f
Lukáš Říha <cedel@centrum.cz>
2018-03-24 18:37:56 +0100

    Tidy up Add Category, Status and Issue admin page

--- ttrackerapp/admin.py
+++ ttrackerapp/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import IssueCategory, IssueStatus, IssueIssue
 
-admin.site.register(IssueCategory)
-admin.site.register(IssueStatus)
-admin.site.register(IssueIssue)
+class IssueCategoryAdmin(admin.ModelAdmin):
+    fields = (('name', 'default'),)
+
+class IssueStatusAdmin(admin.ModelAdmin):
+    fields = (('name', 'default'),)
+
+class IssueIssueAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':
+                    [('reporter', 'owner'),
+                     ('status', 'category'),
+                     ('created', 'is_solved', 'solved')
+                     ]
+                }
+         ),
+        (None, {'fields': ['description']})
+        ] 
+
+admin.site.register(IssueCategory, IssueCategoryAdmin)
+admin.site.register(IssueStatus, IssueStatusAdmin)
+admin.site.register(IssueIssue, IssueIssueAdmin)
