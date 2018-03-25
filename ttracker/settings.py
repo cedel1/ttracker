1baa80e0617c7500af50cf8168ea70ff93992777
Lukáš Říha <cedel@centrum.cz>
2018-03-25 19:26:17 +0200

    Revert "Add admin view permission app and related config"
    This reverts commit 57da481a76edef874ec64ac35119a13cb2cc644a.

--- ttracker/settings.py
+++ ttracker/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_view_permission',
     'ttrackerapp.apps.TtrackerappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,10 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-# Setting which models should recieve the admin_view_permission
-ADMIN_VIEW_PERMISSION_MODELS = [
-    'auth.User',
-    'ttrackerapp.IssueIssue'
-]
